5ea25889a085e708ff87ea511e3ab2367a39ab29
OptimusIacobus <supajake118@gmail.com>
2020-03-19 20:26:44 -0400

    Main overhaul

---
+++ scripts/counter.js
@@ -0,0 +1,18 @@
+﻿function animateValue(id, start, end, duration) {
+    var range = end - start;
+    var current = start;
+    var increment = end > start ? 1 : -1;
+    var stepTime = Math.abs(Math.floor(duration / range));
+    var obj = document.getElementById(id);
+    var timer = setInterval(function () {
+        current += increment;
+        obj.innerHTML = current;
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, stepTime);
+}
+
+animateValue("value", 0, 10, 1000);
+animateValue("value2", 0, 2, 500);
+animateValue("value3", 0, 247, 1000);
